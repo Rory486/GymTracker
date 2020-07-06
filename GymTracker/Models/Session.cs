689fa3f3fbbafcf4e0e3099f47bda76079a7bc04
Rory <rory.price486@gmail.com>
2020-07-06 21:01:03 +0100

    Building data access service
    Created in-memory example data for testing.
    Created interface to allow easy switching of data source later.
    Created separate class library to store data access layer code.

--- GymTracker/Models/Session.cs
+++ GymTracker/Models/Session.cs
@@ -8,7 +8,7 @@ namespace GymTracker.Models
     public class Session
     {
         public int SessionId { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public DateTime Date { get; set; }
     }
 }
--- GymTracker/Pages/SessionList.cshtml.cs
+++ GymTracker/Pages/SessionList.cshtml.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using GymTracker.Data;
 
 namespace GymTracker.Pages
 {
     public class SessionListModel : PageModel
     {
         private ILogger<IndexModel> _logger;
+        private readonly ISessionData sessionData;
 
-        public SessionListModel(ILogger<IndexModel> logger)
+        public SessionListModel(ISessionData sessionData, ILogger<IndexModel> logger)
         {
             _logger = logger;
+            this.sessionData = sessionData;
         }
         public void OnGet()
         {
